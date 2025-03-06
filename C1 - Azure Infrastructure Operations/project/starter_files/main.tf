0c20092470882d2cc1a765f7eaeba90ce4235e46
rahmed <rania.k.zaghloul@gmail.com>
2025-03-06 02:40:58 +0200

    change static values

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -45,8 +45,8 @@ resource "azurerm_linux_virtual_machine" "main" {
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
   size                            = "Standard_D2s_v3"
-  admin_username                  = "odl_user_274883"
-  admin_password                  = "Rania@2025"
+  admin_username                  = "userid"
+  admin_password                  = "userpassword"
   disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.main.id,
